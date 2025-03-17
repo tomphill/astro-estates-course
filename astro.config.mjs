5c587e016a0c67d5a28faa4e667155487aef53d0
tom <tomphill@live.co.uk>
2025-03-17 10:01:27 +0800

    Upgrade to astro v5 + tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,29 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import { loadEnv } from "vite";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 import robotsTxt from "astro-robots-txt";
-const {
-  PUBLIC_WP_URL
-} = loadEnv(process.env.NODE_ENV, process.cwd(), "");
-
+const { PUBLIC_WP_URL, PUBLIC_PROTOCOL } = loadEnv(
+  process.env.NODE_ENV,
+  process.cwd(),
+  "",
+);
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-estates-course.vercel.app",
   image: {
-    domains: [PUBLIC_WP_URL]
+    remotePatterns: [
+      {
+        protocol: `${PUBLIC_PROTOCOL}`,
+        hostname: `${PUBLIC_WP_URL}`,
+      },
+    ],
   },
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), react(), robotsTxt()],
-  output: "hybrid",
-  adapter: vercel()
+  integrations: [react(), robotsTxt()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  adapter: vercel(),
 });
